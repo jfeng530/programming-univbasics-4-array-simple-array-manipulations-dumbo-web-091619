30119f3ed042a592eeea9c2026ca88af6e14c0c8
Jacky Feng <jackyfeng530@yahoo.com>
2019-08-23 01:46:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,5 +27,21 @@ def using_concat (array1, array2)
 end
 
 def using_insert (array, element)
-  
+  array.insert(4, element)
+end
+
+def using_uniq (array)
+  array.uniq
+end
+
+def using_flatten (array)
+  array.flatten
+end
+
+def using_delete (array, integer)
+  array.delete(integer)
+end
+
+def using_delete_at (array, integer)
+  array.
 end
